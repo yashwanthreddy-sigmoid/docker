8c08da894f7c2537b4cc3356ade0c6e1307f64a5
yashwanthreddy-sigmoid <98736166+yashwanthreddy-sigmoid@users.noreply.github.com>
2022-04-01 11:27:41 +0530

    Delete kubernetes_files directory

--- kubernetes_files/script-create.sh
+++
@@ -1,4 +0,0 @@
-kubectl create -f deployment-puckel.yml
-kubectl create -f deployment-postgres.yml
-kubectl create -f service-postgres.yml
-kubectl create -f service-puckel.yml
--- kubernetes_files/script-delete.sh
+++
@@ -1,5 +0,0 @@
-
-kubectl delete deploy puckel-deploy
-kubectl delete deploy postgres-deploy
-kubectl delete svc puckel-service
-kubectl delete svc postgres-service-db
